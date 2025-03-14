98354e26623477580aeb891e00a22e204ca1211a
bursaf2 <asim.ozturk.24635@ozu.edu.tr>
2025-03-15 01:09:42 +0300

    Add project files.

---
+++ AjaxMVC/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using AjaxMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class ProductsController : Controller
+{
+    private readonly ProductDbContext _context;
+
+    public ProductsController(ProductDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();  
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Search(string term)
+    {
+        if (string.IsNullOrWhiteSpace(term))
+        {
+            return Json(new { success = false, message = "Search term is empty", data = new List<object>() });
+        }
+
+        var products = await _context.Products
+            .Where(p => p.Name.Contains(term))
+            .Select(p => new { p.Id, p.Name, p.Price })
+            .ToListAsync();
+
+        return Json(new { success = true, data = products });
+    }
+
+
+}
---
+++ AjaxMVC/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AjaxMVC.Data; 
+
+using AjaxMVC.Models; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var productDbConnectionString = builder.Configuration.GetConnectionString("ProductDbConnection")
+    ?? throw new InvalidOperationException("Connection string 'ProductDbConnection' not found.");
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlServer(productDbConnectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();  
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Products}/{action=Search}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
